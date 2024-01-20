7a9ee59d80fad6155f0e513ecf1be0dcccdf83bf
qustoo <zalevskiy@sfedu.ru>
2024-01-20 04:39:17 +0300

    router for export data

--- app/routers/exports.py
+++ app/routers/exports.py
@@ -1,18 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from utils import DataWriter
-from dao import MongoDAO
+from dao import get_mongo_database, MongoImagesDAO
 
-router = APIRouter(prefix="/export")
+router = APIRouter(prefix="/export", tags=["Exports"])
 
 
 @router.get("/")
-async def export_data(filename: str = "data.xlsx"):
-    data = await MongoDAO.get_data()
-    columns = await MongoDAO.get_columns()
+async def export_data(
+    database: MongoImagesDAO = Depends(get_mongo_database), filename: str = "data.xlsx"
+):
+    data = await database.get_data()
+    columns = await database.get_columns()
     try:
         with DataWriter(columns, filename) as dw:
             dw.write_columns()
             dw.write_values(data)
     except Exception as err:
         return HTTPException(status_code=400, detail=str(err))
+    return JSONResponse("data.xsls was created")
