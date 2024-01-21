1cb112384c7eb487f8de0ba3e453d77a043c9b0d
qustoo <zalevskiy@sfedu.ru>
2024-01-21 03:02:36 +0300

    depends injection the validate id to object id

---
+++ app/dependecies.py
@@ -0,0 +1,21 @@
+from bson import ObjectId
+from bson.errors import InvalidId
+from exceptions import InvalidBsonIDException
+from fastapi import Path
+from typing_extensions import Annotated
+
+
+async def validate_id(user_id: Annotated[str, Path()]):
+    try:
+        bson_id = ObjectId(user_id)
+    except (InvalidId, TypeError):
+        raise InvalidBsonIDException()
+    return bson_id
+
+
+async def validate_file_id(file_id: Annotated[str, Path()]):
+    try:
+        bson_id = ObjectId(file_id)
+    except (InvalidId, TypeError):
+        raise InvalidBsonIDException()
+    return bson_id
