111e259e96015ed697b3d60cad1624d5cd172db6
qustoo <zalevskiy@sfedu.ru>
2024-01-21 22:38:27 +0300

    add redis cache

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import uvicorn
-from config import settings
 from fastapi import FastAPI
-from routers import export_router, image_router, users_router
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+
+from app.config import settings
+from app.routers import export_router, image_router, users_router
 
 app = FastAPI(title="Test Task", version="0.0.1", debug=True)
 
@@ -9,14 +13,14 @@ app.include_router(users_router)
 app.include_router(export_router)
 app.include_router(image_router)
 
+
 # REDIS
-# from fastapi_cache import FastAPICache
-# from fastapi_cache.backends.redis import RedisBackend
-# from redis import asyncio as aioredis
-# @app.on_event("startup")
-# async def startup_event():
-#     redis = aioredis.from_url(f"redis://{settings.REDIS_HOST:}",encoding="utf-8",decode_responses= True)
-#     FastAPICache.init(RedisBackend(redis),prefix="test_task_cache")
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST:}", encoding="utf-8", decode_responses=True
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="test_task_cache")
 
 
 if __name__ == "__main__":
