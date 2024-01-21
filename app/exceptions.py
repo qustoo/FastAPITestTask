97b435cf66ba9be81681ea421f3242d2e903a332
qustoo <zalevskiy@sfedu.ru>
2024-01-21 03:02:53 +0300

    modify inherance exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,11 +1,9 @@
-from bson import ObjectId
-from bson.errors import InvalidId
 from fastapi import status
 from fastapi.exceptions import HTTPException
 
 
 class ItemNotFoundException(HTTPException):
-    status_code = 404
+    status_code = status.HTTP_404_NOT_FOUND
     detail = ""
 
     def __init__(self):
@@ -13,17 +11,15 @@ class ItemNotFoundException(HTTPException):
 
 
 class UserNotFoundException(ItemNotFoundException):
-    status_code = status.HTTP_404_NOT_FOUND
     detail = "User not found"
 
 
 class FileNotFoundException(ItemNotFoundException):
-    status_code = status.HTTP_404_NOT_FOUND
     detail = "File not found"
 
 
 class InvalidBsonIDException(HTTPException):
-    status_code: int = 422
+    status_code: int = status.HTTP_422_UNPROCESSABLE_ENTITY
     detail: str = "Incorrect input id"
 
     def __init__(self):
@@ -31,16 +27,8 @@ class InvalidBsonIDException(HTTPException):
 
 
 class IncorrectVoteValueException(HTTPException):
-    status_code: int = (404,)
-    detail: str = ("incorrect vote value(less then 0 or greaten then 10)",)
+    status_code: int = status.HTTP_422_UNPROCESSABLE_ENTITY
+    detail: str = "incorrect vote value(less then 0 or greaten then 10)"
 
     def __init__(self):
         super().__init__(status_code=self.status_code, detail=self.detail)
-
-
-def ValidateBsonID(id: str):
-    try:
-        bson_id = ObjectId(id)
-    except (InvalidId, TypeError):
-        raise InvalidBsonIDException()
-    return bson_id
