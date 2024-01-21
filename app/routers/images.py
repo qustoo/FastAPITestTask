96fa7ce98606a1106be32aa98d3a5d125361ac45
qustoo <zalevskiy@sfedu.ru>
2024-01-21 03:03:54 +0300

    depends injection validate id in images routers

--- app/routers/images.py
+++ app/routers/images.py
@@ -1,7 +1,9 @@
+from bson import ObjectId
 from dao.grid_fs_mongo_dao import MongoImagesDAO
 from database import get_mongo_database
-from exceptions import FileNotFoundException, ValidateBsonID
-from fastapi import APIRouter, Depends, Path
+from dependecies import validate_file_id
+from exceptions import FileNotFoundException
+from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
 from schemas.images import ImageModel
 
@@ -10,22 +12,20 @@ router = APIRouter(prefix="/files", tags=["Images"])
 
 @router.get("/information/{file_id}", response_model=ImageModel)
 async def get_file_information(
-    database: MongoImagesDAO = Depends(get_mongo_database), file_id: str = Path(...)
+    database: MongoImagesDAO = Depends(get_mongo_database),
+    file_id: ObjectId = Depends(validate_file_id),
 ):
-    file_id = ValidateBsonID(file_id)
-    file = await database.get_file_information(file_id)
-    if file:
+    if file := await database.get_file_information(file_id):
         return file
     raise FileNotFoundException()
 
 
 @router.get("/download_file/{file_id}")
 async def download_file(
-    database: MongoImagesDAO = Depends(get_mongo_database), file_id: str = Path(...)
+    database: MongoImagesDAO = Depends(get_mongo_database),
+    file_id: ObjectId = Depends(validate_file_id),
 ):
-    file_id = ValidateBsonID(file_id)
-    file = await database.get_file_object(file_id)
-    if file:
+    if file := await database.get_file_object(file_id):
         return StreamingResponse(
             database.download_stream(file_id), media_type="image/png"
         )
